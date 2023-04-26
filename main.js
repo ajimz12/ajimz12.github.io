0004d0297b1e8a3608df906659d774d061bb7086
Álvaro <alvarojimgil12@gmail.com>
2023-04-26 21:08:18 +0200

    FINISH navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".navbar")
+
+navToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("navbar_visible")
+
+})
